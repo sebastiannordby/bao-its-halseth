a914377e16d242f8c2aed9ed9b36839160bec2e1
Sebastian <sebastian.nordby.bjornstad@wlcom.no>
2024-01-24 17:15:21 +0100

    Start remodelling existing data structure

---
+++ CIS.Domain/Products/ProductGroup.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIS.Domain.Products
+{
+    public class ProductGroup
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+    }
+}
