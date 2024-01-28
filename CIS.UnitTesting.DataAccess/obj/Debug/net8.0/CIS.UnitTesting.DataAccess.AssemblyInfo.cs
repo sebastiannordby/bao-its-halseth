2cc147d16e771559318f9dde77369e6d2a8909fa
Zaphand <andreas@halseth.no>
2024-01-28 04:15:15 +0100

    radzen misc removed

--- CIS.UnitTesting.DataAccess/obj/Debug/net8.0/CIS.UnitTesting.DataAccess.AssemblyInfo.cs
+++ CIS.UnitTesting.DataAccess/obj/Debug/net8.0/CIS.UnitTesting.DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CIS.UnitTesting.DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+824c88ad0f9b979cca8e001baa265635e8380d11")]
 [assembly: System.Reflection.AssemblyProductAttribute("CIS.UnitTesting.DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CIS.UnitTesting.DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
