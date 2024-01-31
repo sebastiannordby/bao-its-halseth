819911017afbfa2ccdcd954b0483b18f9713dd20
Zaphand <andreas@halseth.no>
2024-02-01 00:54:57 +0100

    import service, extension helper class

---
+++ CIS/Extensions/StringExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace CIS.Extensions
+{
+    public static class StringExtensions
+    {
+
+        public static decimal? StringDecimalToDecimal(this string value)
+        {
+            if (value is null)
+                return null;
+
+            if (value == "NULL")
+                return null;
+
+            return Convert.ToDecimal(value);
+        }
+
+    }
+}
