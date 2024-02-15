256caebfc104186aebcf75201c9d72493a0002da
Zaphand <andreas@halseth.no>
2024-02-15 23:08:52 +0100

    tailwind

--- CIS.WebApp/tailwind.config.js
+++ CIS.WebApp/tailwind.config.js
@@ -1,9 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        "./Components/Layout/*.razor",
-        "./Components/Components/*.razor",
-        "./Components/Pages/*.razor",
+        "./Components/**/*.razor",
         "./Components/*.razor"
     ],
     theme: {
