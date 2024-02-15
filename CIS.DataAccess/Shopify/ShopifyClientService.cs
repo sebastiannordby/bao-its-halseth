0dd553537769364807e2718448e10a8b66fe423e
Zaphand <andreas@halseth.no>
2024-02-15 21:27:10 +0100

    rebase

---
+++ CIS.DataAccess/Shopify/ShopifyClientService.cs
@@ -0,0 +1,37 @@
+﻿using CIS.Application.Shopify.Options;
+using Microsoft.Extensions.Options;
+using ShopifySharp;
+using ShopifySharp.Credentials;
+using ShopifySharp.Factories;
+
+namespace CIS.Application.Shopify
+{
+    public class ShopifyClientService
+    {
+        private readonly ShopifyClientServiceOptions _options;
+        private readonly IOrderServiceFactory _orderServiceFactory;
+        private readonly ShopifyApiCredentials _apiCredentials;
+        private readonly IOrderService _orderService;
+
+
+        public ShopifyClientService(IOptions<ShopifyClientServiceOptions> options, IOrderServiceFactory orderServiceFactory)
+        {
+            _options = options.Value;
+            _orderServiceFactory = orderServiceFactory;
+
+            //trenger accesstoken :)))))
+            _apiCredentials = new ShopifyApiCredentials("rcordre", "");
+            _orderService = _orderServiceFactory.Create(_apiCredentials);
+
+        }
+
+
+        public async Task<IEnumerable<Order>> GetOrders()
+        {
+            var orders = await _orderService.ListAsync();
+
+            return orders.Items;
+        }
+
+    }
+}
