27b86eadf5c77a7f45fbd2b31cefdc68a91ef322
Sebastian <sebastianbjornstad@hotmail.com>
2024-03-10 11:39:04 +0100

    Upgrading login to V2

--- Infrastructure/backend.tf
+++ Infrastructure/backend.tf
@@ -10,7 +10,7 @@ terraform {
       resource_group_name = var.backend_resource_group_name 
       storage_account_name = var.backend_storage_account_name 
       container_name = var.backend_container_name 
-      key =  var.backend_key 
+      key = var.backend_key 
   }
 }
 
