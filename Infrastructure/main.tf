14c4f65f5efeb5143059e398355ad6d5bdee20eb
Sebastian Nordby <sebastianbjornstad@hotmail.com>
2024-03-21 20:44:27 +0100

    Update main.tf (#28)

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -53,7 +53,7 @@ locals {
 }
 
 resource "azurerm_linux_web_app" "cisappservice" {
-  name = "cis-app-service"
+  name = "cis-appservice"
   location = data.azurerm_resource_group.cis.location
   resource_group_name = data.azurerm_resource_group.cis.name
   service_plan_id = azurerm_app_service_plan.cisappserviceplan.id
